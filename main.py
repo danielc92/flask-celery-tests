5bed01f79686a8c6f475eee3ed29c778974ce12b
Daniel Corcoran <danielc.92@outlook.com>
2019-03-28 11:17:30 +1100

    Changed backend broker to REDIS

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask
+from flask_celery_ import make_celery
 from flask_sqlalchemy import SQLAlchemy
 from random import choice
 from string import ascii_lowercase
-
+from datetime import datetime
 
 app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = 'amqb://localhost//'
-app.config['CELERY_BACKEND'] = 'postgresql://postgres:12345@localhost:5432/test'
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:12345@localhost:5432/test'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+celery = make_celery(app)
 db = SQLAlchemy(app)
 
 
@@ -19,24 +22,32 @@ class Results(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
     data = db.Column('data', db.String(100))
 
+# Drop the table(s) in schema and recreate it
+db.drop_all()
 db.create_all()
 
-
+@celery.task(name='main.insert')
 def insert():
 
-    for num in range(1000):
-        data = ''.join(choice(ascii_lowercase)) for i in range(20)
+    for num in range(50000):
+        data = ''.join([choice(ascii_lowercase) for i in range(20)])
         result = Results(data=data)
         db.session.add(result)
 
     db.session.commit()
+
+
     return 'Commit Successful.'
 
 
 @app.route('/process')
 def process():
 
-    return '<code>async request has been successfully sent.</code>'
+    start = datetime.now()
+    insert.delay()
+    end = datetime.now() - start
+
+    return '<code style="font-size: 3rem; color: green;">request has been successfully sent. finished in {}</code>'.format(end)
 
 if __name__ == '__main__':
     app.run(debug=True)
